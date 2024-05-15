be1a4f70dc15dd4d0c80f383f84f9a965b9edab7
louis <18758398381@163.com>
2024-05-15 23:03:36 +0800

    rpc学习

---
+++ http_rpc_test/client/client.go
@@ -0,0 +1,14 @@
+package main
+
+type HelloService struct {
+}
+
+func (s *HelloService) Hello(request string, reply *string) error {
+
+	*reply = "hello " + request
+	return nil
+}
+
+func main() {
+
+}
